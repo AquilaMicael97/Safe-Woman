afd7f0c48cc77f17f5ddb663fca2f54ebaa7581f
Aquila Micael <aquila_micael@hotmail.com>
2026-06-09 21:38:31 -0300

    Remove arquivos desnecessários: static legado, imagens de teste, Docker, schema duplicado

--- testechave.py
+++
@@ -1,7 +0,0 @@
-import os
-from google.cloud import vision
-
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\BIOPARK\MariaPenha\chave.json"
-
-client = vision.ImageAnnotatorClient()
-print("Conexão com a Vision API estabelecida com sucesso!")
